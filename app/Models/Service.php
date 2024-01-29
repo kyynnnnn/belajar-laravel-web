64a1302d84c58a21a6622fd472001b058d19b6c7
rdtyaa <maoreendsafiraa@gmail.com>
2024-01-29 18:16:18 +0700

    create model and giving eloquent to user, detail_user, service, order

---
+++ app/Models/Service.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Service extends Model
+{
+    // use HasFactory;
+    use SoftDeletes;
+
+    public $table = 'service';
+
+    protected $dates = [
+        'updated_at',
+        'created_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'users_id',
+        'title',
+        'decription',
+        'delivery_time',
+        'revision_limit',
+        'price',
+        'note',
+        'updated_at',
+        'created_at',
+        'deleted_at',
+    ];
+
+
+    // one to many
+    public function user()
+    {
+        return $this->belongsTo('App/Models/User', 'users_id', 'id');
+    }
+}
